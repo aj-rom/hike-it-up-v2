01dbc7a18aaaf7b61aea085563d2e78046ee347d
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-06 16:10:01 -0400

    Add browser router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import TrailsContainer from "./containers/TrailsContainer";
 
 export default function App() {
-  return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-      </React.Fragment>
-  )
+    return (
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/trails">Trails</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/trails">
+                        <TrailsContainer />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
 }
