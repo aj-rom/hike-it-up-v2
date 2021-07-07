476a19709df20e5d5837e05f7b7eb6764f23f2d9
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-07 06:50:17 -0400

    add hours to trails

--- db/migrate/20210706205906_create_trails.rb
+++ db/migrate/20210706205906_create_trails.rb
@@ -3,6 +3,8 @@ class CreateTrails < ActiveRecord::Migration[6.1]
     create_table :trails do |t|
       t.text :name
       t.text :description
+      t.integer :hour_open
+      t.integer :hour_close
       t.belongs_to :user
 
       t.timestamps
