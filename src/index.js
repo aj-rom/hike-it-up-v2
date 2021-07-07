54b5453f77e040b318a276c858d7052a5c68f0f7
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-06 23:58:56 -0400

    create store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import {Provider} from "react-redux";
+import {compose, applyMiddleware, createStore} from 'redux';
+import trailsReducer from "./reducers/trailsReducer";
+import thunk from "redux-thunk";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(trailsReducer, composeEnhancer(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
