f67c35aeba1f3bf8cea877c552caec3be3cdf3af
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-07 06:50:17 -0400

    add hours to trails

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# user = User.create(name: 'ayo', email: 'ayo@gmail.com', password: 'Testing', password_confirmation: 'Testing')
-#
-# user.trails.build(name: 'Test Trail #1', description: 'Test trail #1 description').save
+user = User.create(name: 'ayo', email: 'ayo@gmail.com', password: 'Testing', password_confirmation: 'Testing')
 
-User.first.trails.build(name: 'Test Trail #2', description: 'Test Trail #2 description').save
+user.trails.build(name: 'Test Trail #1', description: 'Test trail #1 description', hour_open: 6, hour_close: 7).save
+
+User.first.trails.build(name: 'Test Trail #2', description: 'Test Trail #2 description', hour_open: 8, hour_close: 9).save
