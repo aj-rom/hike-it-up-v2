ee9fde0acb71953e6fd85f9e4b6a3095ee183da1
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-07 18:17:10 -0400

    add user_id

--- app/serializers/trails_serializer.rb
+++ app/serializers/trails_serializer.rb
@@ -1,4 +1,4 @@
 class TrailsSerializer < ApplicationSerializer
-  attributes :id, :name, :description, :hour_open, :hour_close
-  belongs_to :user
+  attributes :id, :name, :description, :hour_open, :hour_close, :user_id
+  set_type :trail
 end
