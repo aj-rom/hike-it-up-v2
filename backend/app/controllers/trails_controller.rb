82e78a9ce3c12af7b0e61987b500c0ba2594d17e
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-06 22:45:04 -0400

    render trails as json

--- backend/app/controllers/trails_controller.rb
+++ backend/app/controllers/trails_controller.rb
@@ -1,23 +1,26 @@
 class TrailsController < ApplicationController
 
-    def index
+  def index
+    trails = Trail.all
+    trails_json = TrailsSerializer.new(trails).serialized_json
+    render json: trails_json
+  end
 
-    end
+  def show
+    trail = Trail.find_by(id: params[:id])
+    render json: TrailSerializer.new(trail).serialized_json
+  end
 
-    def show
+  def create; end
 
-    end
+  def destroy; end
 
-    def create
+  def update; end
 
-    end
+  private
 
-    def destroy
-
-    end
-
-    def update
-
-    end
+  def trail_params
+    params.require(:trail).permit(:title, :description, :images)
+  end
 
 end
