3d918a8a663978eedc27eca73df2eb6617da54cb
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-07 18:17:37 -0400

    use sanitized json

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,17 +1,27 @@
 class TrailsController < ApplicationController
+  before_action
 
   def index
     trails = Trail.all
-    trails_json = TrailsSerializer.new(trails).serialized_json
+    trails_json = TrailsSerializer.new(trails).to_h
     render json: trails_json
   end
 
   def show
     trail = Trail.find_by(id: params[:id])
-    render json: TrailSerializer.new(trail).serialized_json
+    render json: TrailSerializer.new(trail).to_h
   end
 
-  def create; end
+  def create
+    trail = Trail.create(trail_params)
+    if trail
+      puts 'Created...'
+      puts trail.attributes
+      redirect_to trail_path(trail)
+    else
+      render json: trail.errors.full_messages
+    end
+  end
 
   def destroy; end
 
@@ -20,7 +30,9 @@ class TrailsController < ApplicationController
   private
 
   def trail_params
-    params.require(:trail).permit(:title, :description, :images)
+    puts "Trail Params"
+    puts params[:trail]
+    params.require(:trail).permit(:name, :description, :hour_open, :hour_close, :images, :user_id)
   end
 
 end
