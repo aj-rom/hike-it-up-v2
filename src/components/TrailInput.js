6765167af3a1139de521a025c9e5c2cc04d70bb1
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-07 18:32:49 -0400

    switch to card layout

--- src/components/TrailInput.js
+++ src/components/TrailInput.js
@@ -31,70 +31,74 @@ export default class TrailInput extends Component {
         return (
             <details>
                 <summary>Add A Trail</summary>
-                <form id='create-trail' onSubmit={ e => this.handleSubmit(e)}>
-                    <h2>Create Trail</h2>
-                    <label>
-                        Trail Name
-                        <input type='text'
-                               name='name'
-                               onChange={e => this.handleChange(e)}
-                               value={this.state.name}
-                               required={true}
-                               max={100}
-                               min={3}
-                        />
-                    </label>
+                <article>
+                    <form id='create-trail' onSubmit={ e => this.handleSubmit(e)}>
+                        <header>
+                            <h3>Create Trail</h3>
+                        </header>
+                        <label>
+                            Trail Name
+                            <input type='text'
+                                   name='name'
+                                   onChange={e => this.handleChange(e)}
+                                   value={this.state.name}
+                                   required={true}
+                                   max={100}
+                                   min={3}
+                            />
+                        </label>
 
-                    <label>
-                        Description
-                        <input type='textarea'
-                               name='description'
-                               onChange={e => this.handleChange(e)}
-                               value={this.state.description}
-                               required={true}
-                               max={300}
-                               min={15}
-                        />
-                    </label>
+                        <label>
+                            Description
+                            <input type='textarea'
+                                   name='description'
+                                   onChange={e => this.handleChange(e)}
+                                   value={this.state.description}
+                                   required={true}
+                                   max={300}
+                                   min={15}
+                            />
+                        </label>
 
-                    <label>
-                        Open Time
-                        <input
-                            name='hour_open'
-                            type='number'
-                            max={12}
-                            min={1}
-                            onChange={e => this.handleChange(e)}
-                            value={this.state.hour_open}
-                            required={true}
-                        />
-                    </label>
+                        <label>
+                            Open Time
+                            <input
+                                name='hour_open'
+                                type='number'
+                                max={12}
+                                min={1}
+                                onChange={e => this.handleChange(e)}
+                                value={this.state.hour_open}
+                                required={true}
+                            />
+                        </label>
 
-                    <label>
-                        Close Time
-                        <input
-                            name='hour_close'
-                            type='number'
-                            max={12}
-                            min={1}
-                            onChange={e => this.handleChange(e)}
-                            value={this.state.hour_close}
-                            required={true}
-                        />
-                    </label>
+                        <label>
+                            Close Time
+                            <input
+                                name='hour_close'
+                                type='number'
+                                max={12}
+                                min={1}
+                                onChange={e => this.handleChange(e)}
+                                value={this.state.hour_close}
+                                required={true}
+                            />
+                        </label>
 
-                    <label>
-                        Images
-                        <input type='file'
-                               name='images'
-                               onChange={e => this.handleChange(e)}
-                               value={this.state.images}
-                        />
-                    </label>
+                        <label>
+                            Images
+                            <input type='file'
+                                   name='images'
+                                   onChange={e => this.handleChange(e)}
+                                   value={this.state.images}
+                            />
+                        </label>
 
-                    <input type='submit' value='Create Trail'/>
+                        <input type='submit' value='Create Trail'/>
 
-                </form>
+                    </form>
+                </article>
             </details>
         )
     }
