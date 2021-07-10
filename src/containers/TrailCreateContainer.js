d1ee3cd2e6118426ded39c08808033bb9afe7b9c
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-10 18:59:46 -0400

    refactor create component

---
+++ src/containers/TrailCreateContainer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import TrailInput from "../components/TrailInput";
+import { createTrail } from "../actions/trailActions";
+import { connect } from "react-redux";
+
+const TrailCreateContainer = () => {
+    return (
+        <React.Fragment>
+            <hgroup>
+                <h1>Add a Trail</h1>
+                <h3>Post trails you've been on!</h3>
+            </hgroup>
+            <section>
+                <TrailInput createTrail={this.props.createTrail}/>
+            </section>
+        </React.Fragment>
+    )
+}
+
+const dispatch = ({ createTrail })
+
+export default connect( null, dispatch)(TrailCreateContainer)
