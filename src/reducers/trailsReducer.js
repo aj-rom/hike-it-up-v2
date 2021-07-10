2cfad32bc38636804a929253b452e536e8083184
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-10 19:01:56 -0400

    add reducer for focused trail

--- src/reducers/trailsReducer.js
+++ src/reducers/trailsReducer.js
@@ -1,4 +1,4 @@
-const trailsReducer = ( state = { trails: [], isLoaded: false, error: null }, action) => {
+const trailsReducer = ( state = { trail: {}, trails: [], isLoaded: false, error: null }, action) => {
     switch(action.type) {
         case 'LOADING_TRAILS':
             return { ...state, trails: state.trails, isLoaded: false }
@@ -6,9 +6,11 @@ const trailsReducer = ( state = { trails: [], isLoaded: false, error: null }, ac
             return { ...state, trails: [...state.trails, action.trail], isLoaded: true }
         case 'FETCH_TRAILS':
             return { ...state, trails: action.trails, isLoaded: true}
+        case 'LOADING_TRAIL':
+            return { ...state, trail: state.trail, isLoaded: false }
+        case 'FETCH_TRAIL':
+            return { ...state, trail: action.trail, isLoaded: true}
         case 'ERROR':
-            console.log('Action', action)
-            console.log('An Error has occurred', action.error)
             return { ...state, error: action.error, isLoaded: false }
         default:
             return state
