6dd0fd69c16113afec1011c00349ee16370306b6
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-10 20:40:20 -0400

    add framework table & stylize

--- src/components/static/About.js
+++ src/components/static/About.js
@@ -1,29 +1,32 @@
 // A little bit about the development. Link to blog post, github, and linkedIn
 
 import React from 'react'
+import FrameWorkTable from "./about/FrameWorkTable";
 
 export default function About() {
-
     return (
         <section id='about'>
             <hgroup>
                 <h2>About</h2>
                 <h4>A little bit about this project.</h4>
             </hgroup>
-            <p>This is some page content.</p>
-
-            <article>
-                <header>
-                    <h3>FrameWorks Used</h3>
-                </header>
-                <ul>
-                    <li>Ruby on Rails</li>
-                    <li>React.js</li>
-                    <li>React-Redux</li>
-                    <li>Redux-thunk</li>
-                    <li>PicoCSS</li>
-                </ul>
-            </article>
+            <section>
+                <article className='grid'>
+                    <section>
+                        <h5>My Motivation</h5>
+                        <p>A little bit about <i>why</i> I wanted to rebuild and create <code>hike-it-up</code> in the first place.</p>
+                    </section>
+                    <section>
+                        <h5>Development Process</h5>
+                        <p>Maybe go over the development process.</p>
+                        <ul>
+                            <li>And touch on some features</li>
+                            <li>or link to somewhere</li>
+                        </ul>
+                    </section>
+                </article>
+            </section>
+            <FrameWorkTable/>
         </section>
     )
 }
