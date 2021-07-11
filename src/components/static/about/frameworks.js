6dda1ad7e5b8afad62f453bf9e4b8fd24d5c728b
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-10 20:40:10 -0400

    create framework table data

---
+++ src/components/static/about/frameworks.js
@@ -0,0 +1,41 @@
+const rubyOnRailsInfo = {
+    name: 'Ruby On Rails',
+    desc: 'Used for backend infrastructure and API. Handles HTTP requests and powers everything under the hood.',
+    url: 'https://rubyonrails.org',
+    alt: 'ruby-on-rails',
+    src: 'https://img.icons8.com/color/48/000000/ruby-programming-language.png'
+}
+
+const reactInfo = {
+    name: 'React.js',
+    desc: 'Used for the frontend website. This handles structuring our API data from the backend and allows us to create a beautiful user interface easily.',
+    url: 'https://reactjs.org/',
+    alt: 'react.js',
+    src: 'https://img.icons8.com/office/480/000000/react.png'
+}
+
+const picoCssInfo = {
+    name: 'Pico.css',
+    desc: 'Framework used for styling all of this application, using both their CSS classes and classless version.',
+    url: 'https://picocss.com/',
+    alt: 'pico css image',
+    src: 'https://img.icons8.com/ios-filled/50/4a90e2/css.png'
+}
+
+const icons8Info = {
+    name: 'Icons8',
+    desc: 'Used to get these icons for completely free.',
+    url: 'https://icons8.com',
+    alt: 'icons eight',
+    src: 'https://img.icons8.com/clouds/200/000000/icons8-new-logo.png'
+}
+
+const STANDARD = {
+    name: '',
+    desc: '',
+    url: '',
+    alt: '',
+    src: ''
+}
+
+export const frameworks = [ rubyOnRailsInfo, reactInfo, picoCssInfo, icons8Info ]
