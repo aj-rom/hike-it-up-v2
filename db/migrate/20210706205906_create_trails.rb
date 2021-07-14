63c9ad075291bf3c3085340b30c046d8c8032a59
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-14 10:10:18 -0600

    add address to trails

--- db/migrate/20210706205906_create_trails.rb
+++ db/migrate/20210706205906_create_trails.rb
@@ -3,9 +3,9 @@ class CreateTrails < ActiveRecord::Migration[6.1]
     create_table :trails do |t|
       t.text :name, required: true
       t.text :description, required: true
+      t.text :address, required: true
       t.string :open_at, required: true
       t.string :close_at, required: true
-      t.belongs_to :user, foreign_key: true, index: true
 
       t.timestamps
     end
