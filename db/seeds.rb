199eaf9b58d6f168666d04a774440ea811dd2d32
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-14 10:45:18 -0600

    fix address seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,19 @@
-admin_account = {
-  name: 'ayo',
-  email: 'ayo@gmail.com',
-  password: 'Testing',
-  password_confirmation: 'Testing'
-}
-
-user ||= User.create(admin_account)
-
 trail_one = {
   name: 'Test Trail #1',
   description: 'Test trail #1 description',
-  address: { street: 'Flagstaff Summit', city: 'Boulder', state: 'CO', zipcode: 80303 },
+  address: Address.create(street: 'Flagstaff Summit', city: 'Boulder', state: 'CO', zipcode: 80303),
   open_at: '8:30',
   close_at: '20:00'
 }
 
-user.trails.build(trail_one).save
+Trail.create(trail_one)
 
 trail_two = {
   name: 'Test Trail #2',
   description: 'Test Trail #2 description',
-  address: { street: 'Artist Point', city: 'Boulder', state: 'CO', zipcode: 80303 },
+  address: Address.create(street: 'Artist Point', city: 'Boulder', state: 'CO', zipcode: 80303),
   open_at: '10:30',
   close_at: '22:30'
 }
 
-user.trails.build(trail_two).save
+Trail.create(trail_two)
