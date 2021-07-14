6f2322721d0abeb387627d5052dfea360f8b9d5a
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-14 10:52:22 -0600

    Add validations to address

--- backend/app/models/address.rb
+++ backend/app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+  belongs_to :trail, dependent: :destroy
+
+  validates_presence_of :street, :city, :state, :zipcode
+
+  validates_numericality_of :zipcode
+  validates_length_of :state, maximum: 2
+  validates_length_of :zipcode, maximum: 5
 end
