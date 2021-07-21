18ef29c0c62b7adefe023ab870e85b0b5bba43da
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-21 12:42:25 -0600

    Add logic to AccountContainer.js

--- src/containers/AccountContainer.js
+++ src/containers/AccountContainer.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import {UserInfo} from "../components/account/UserInfo";
+import {Link} from "react-router-dom";
 
 class AccountContainer extends Component {
 
     render() {
-       if (this.props.user_id === null) {
-           return <div>Log In or Create an Account!</div>
-       } else {
-           return <UserInfo user={this.props.user}/>
-       }
+        const { isLoggedIn, user } = this.props
+        if (isLoggedIn) {
+            return <UserInfo user={user}/>
+        } else {
+            return (
+                <div>
+                    <Link to='/login'>Login</Link> or <Link to='/signup'>Sign Up</Link> to get started!
+                </div>
+            )
+        }
     }
 }
 
 const mapStateToProps = (state) => ({
-    user: state.accountReducer.user
+    user: state.accountReducer.user,
+    isLoggedIn: state.accountReducer.isLoggedIn
 })
 
 const dispatch = ({  })
