01e67d61ebf31075bd2faad5c98b3bebc10790a8
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-21 13:17:46 -0600

    add belongs_to user

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Trail < ApplicationRecord
-  # Active Storage
+  belongs_to :user, dependent: :destroy
   has_one :address
 
   # Check for presence of attributes
