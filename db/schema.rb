6aad16f8ec016cf0a70c268770560465c32fb363
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-21 13:17:30 -0600

    create users for devise

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,22 @@ ActiveRecord::Schema.define(version: 2021_07_14_162925) do
     t.text "description"
     t.string "open_at"
     t.string "close_at"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_trails_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "trails"
