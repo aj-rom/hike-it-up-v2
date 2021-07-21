ce749b781cb0cb50ab3b4d974c11a33bfda5e21b
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-21 14:53:01 -0600

    add email validation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :trails
+
+  validates :email, email: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
