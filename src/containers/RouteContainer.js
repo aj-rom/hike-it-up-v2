ccff29f50ba6f23d44f84a295b78b407f2686f4a
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-21 12:42:04 -0600

    add signup and login routes to RouteContainer.js

--- src/containers/RouteContainer.js
+++ src/containers/RouteContainer.js
@@ -5,12 +5,20 @@ import Home from "../components/static/Home";
 import TrailSearchContainer from "./TrailSearchContainer";
 import TrailInspectContainer from "./TrailInspectContainer";
 import TrailCreateContainer from "./TrailCreateContainer";
+import LogInContainer from "./LogInContainer";
+import SignUpContainer from "./SignUpContainer";
 
 export default function RouteContainer() {
     return (
         <Switch>
             <Route exact path="/about">
                 <About />
+            </Route>
+            <Route exact path='/login'>
+                <LogInContainer/>
+            </Route>
+            <Route exact path='/signup'>
+                <SignUpContainer/>
             </Route>
             <Route exact path="/trails">
                 <TrailSearchContainer />
