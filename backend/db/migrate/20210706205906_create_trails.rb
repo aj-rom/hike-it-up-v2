c86ff4009fef1c5ff9eecdbafd9903e922783338
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-21 13:17:46 -0600

    add belongs_to user

--- backend/db/migrate/20210706205906_create_trails.rb
+++ backend/db/migrate/20210706205906_create_trails.rb
@@ -5,7 +5,7 @@ class CreateTrails < ActiveRecord::Migration[6.1]
       t.text :description, required: true
       t.string :open_at, required: true
       t.string :close_at, required: true
-
+      t.belongs_to :user, required: true
       t.timestamps
     end
   end
