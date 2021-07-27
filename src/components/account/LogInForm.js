3a0385107dee7e771e5cf708cc6221fde75a246e
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-27 08:08:28 -0600

    create login form

---
+++ src/components/account/LogInForm.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+
+export default class LogInForm extends Component {
+
+    constructor(props) {
+        super(props);
+        const initialState = { email: '', password: '' }
+        if (props.includeConfirmation) {
+            this.state =  { ...initialState, password_confirmation: '' }
+        } else {
+            this.state = initialState
+        }
+
+    }
+
+
+    handleChange = e => {
+        const { name, value } = e.target
+        this.setState({ ...this.state, [name]: value })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.onSubmit(this.state)
+        if (this.props.includeConfirmation) {
+            this.setState({ email: '', password: '', password_confirmation: ''})
+        } else {
+            this.setState({ email: '', password: '' })
+        }
+    }
+
+    includesConfirmation() {
+        if (this.props.includeConfirmation) {
+            return (
+                <fieldset>
+                    <label>
+                        Password Conformation
+                        <input
+                            type='password'
+                            name='password_confirmation'
+                            value={this.state.password_confirmation}
+                            onChange={this.handleChange}
+                            required={true}
+                            minLength={6}
+                        />
+                    </label>
+                </fieldset>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <fieldset>
+                    <label>
+                        Email Address
+                        <input
+                            type='email'
+                            name='email'
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required={true}
+                        />
+                    </label>
+                </fieldset>
+                <div className='grid'>
+                    <fieldset>
+                        <label>
+                            Password
+                            <input
+                                type='password'
+                                name='password'
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                                required={true}
+                                minLength={6}
+                            />
+                        </label>
+                    </fieldset>
+                    {this.includesConfirmation()}
+                </div>
+            <input type='submit' value='Login'/>
+        </form>
+        )
+    }
+}
