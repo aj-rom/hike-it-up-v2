b41c30a9630a7f3de2aef83bf0a4290c5cbb20a1
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-28 12:20:06 -0600

    Change errors messages to be more ambiguous

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,12 +2,12 @@ class UserController < ApplicationController
 
   def authenticate
     user = User.find_by(email: user_params[:email])
-    return render json: { errors: ['That email has not yet been used to create an account.'] } if user.nil?
+    return render json: { errors: ['Invalid email or password.'] } if user.nil?
 
     if user.valid_password?(user_params[:password])
       render json: UserSerializer.new(user).to_h
     else
-      render json: { errors: [ 'Invalid Password' ] }
+      render json: { errors: ['Invalid email or password.'] }
     end
   end
 
