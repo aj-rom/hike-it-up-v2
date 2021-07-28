6ed04231c065192213085c5977e416f273ccced3
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-28 12:18:36 -0600

    add SignUp and Error actions to reducer

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -1,10 +1,16 @@
-const initialState = { user: { id: null, username: '', email: ''}, isLoggedIn: false }
+const initialState = { user: { id: null, username: '', email: '', errors: []}, isLoggedIn: false }
 const accountReducer = ( state = initialState, action) => {
     switch (action.type) {
+        case 'SIGN_UP':
+            console.log('Attempting signup', action.user)
+            return { user: action.user, isLoggedIn: true }
         case 'LOG_IN':
+            console.log('Attempting login', action.user)
             return { user: action.user, isLoggedIn: true }
         case 'LOG_OUT':
             return initialState
+        case 'ERROR':
+            return { ...state, errors: action.errors }
         default:
             return state
     }
