6b27a11cc572f3eedea75fce5be8510d389dbba2
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-28 12:19:02 -0600

    implement redirect for logged in users

--- src/containers/RouteContainer.js
+++ src/containers/RouteContainer.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Route, Switch} from "react-router-dom";
+import React, { Component } from 'react'
+import {Route, Switch, Redirect} from "react-router-dom";
 import About from "../components/static/About";
 import Home from "../components/static/Home";
 import TrailSearchContainer from "./TrailSearchContainer";
@@ -7,32 +7,43 @@ import TrailInspectContainer from "./TrailInspectContainer";
 import TrailCreateContainer from "./TrailCreateContainer";
 import LogInContainer from "./LogInContainer";
 import SignUpContainer from "./SignUpContainer";
+import {connect} from "react-redux";
 
-export default function RouteContainer() {
-    return (
-        <Switch>
-            <Route exact path="/about">
-                <About />
-            </Route>
-            <Route exact path='/login'>
-                <LogInContainer/>
-            </Route>
-            <Route exact path='/signup'>
-                <SignUpContainer/>
-            </Route>
-            <Route exact path="/trails">
-                <TrailSearchContainer />
-            </Route>
-            <Route exact path="/new_trail">
-                <TrailCreateContainer/>
-            </Route>
-            <Route exact path="/trails/:id">
-                <TrailInspectContainer/>
-            </Route>
-            <Route exact path="/">
-                <Home />
-            </Route>
-        </Switch>
-    )
+class RouteContainer extends Component {
+
+    render() {
+        const { isLoggedIn } = this.props
+        return (
+            <Switch>
+                <Route exact path="/about">
+                    <About/>
+                </Route>
+                <Route exact path='/login'>
+                    { isLoggedIn ? <Redirect to={'/'}/> : <LogInContainer/>}
+                </Route>
+                <Route exact path='/signup'>
+                    { isLoggedIn ? <Redirect to={'/'}/> : <SignUpContainer/>}
+                </Route>
+                <Route exact path="/trails">
+                    <TrailSearchContainer/>
+                </Route>
+                <Route exact path="/new_trail">
+                    <TrailCreateContainer/>
+                </Route>
+                <Route exact path="/trails/:id">
+                    <TrailInspectContainer/>
+                </Route>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+            </Switch>
+        )
+    }
 }
+
+const mapStateToProps = (state) => ({
+    isLoggedIn: state.accountReducer.isLoggedIn
+})
+
+export default connect(mapStateToProps, null)(RouteContainer)
 
