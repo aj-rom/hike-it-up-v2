3afda07bd63302147c9d72f03f157acfee94067a
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-28 12:18:47 -0600

    add SignUp action

--- src/actions/accountActions.js
+++ src/actions/accountActions.js
@@ -1,6 +1,30 @@
 import {BACKEND_URL, handleError} from "../shared/constants";
 
 export const logOut = () => ({ type: 'LOG_OUT'})
+
+export const signUp = (data) => {
+    const user = { user: data }
+    const config = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    }
+
+    return (dispatch) => {
+        function handleResponse(resp) {
+            if (resp.errors.length > 0) {
+                dispatch({ type: 'ERROR', errors: resp.errors})
+            } else {
+                dispatch({ type: 'SIGN_UP', user: resp })
+            }
+        }
+
+        return fetch(`${BACKEND_URL}sign_up`, config)
+            .then(resp => resp.json())
+            .then(json => handleResponse(json))
+            .catch(e => handleError(e))
+    }
+}
 export const logIn = (data) => {
     const user = { user: data }
     const config = {
@@ -17,7 +41,6 @@ export const logIn = (data) => {
                 dispatch({ type: 'LOG_IN', user: resp })
             }
         }
-
 
         return fetch(`${BACKEND_URL}sign_in`, config)
             .then(e => e.json())
