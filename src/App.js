499b2135f72a8d7b408629b0f1fd92dffa04f7c7
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-30 11:54:05 -0600

    connect navbar to account state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { HashRouter, NavLink } from "react-router-dom";
 import RouteContainer from "./containers/RouteContainer";
+import {connect} from "react-redux";
+import {logOut} from "./actions/accountActions";
 
-export default function App() {
-    return (
-        <HashRouter baseName='hike-it-up-v2'>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/trails">Trails</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about">About</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-                <RouteContainer/>
-            </div>
-        </HashRouter>
-    );
+ class App extends Component {
+
+     handleLogOut = e => {
+         e.preventDefault()
+         return this.props.logOut
+     }
+
+     getSignOutButton = () => {
+         return <button onClick={this.handleLogOut} value='Log Out'/>
+     }
+
+     render() {
+         return (
+             <HashRouter baseName='hike-it-up-v2'>
+                 <div>
+                     <nav>
+                         <ul>
+                             <li>
+                                 <NavLink to="/">Home</NavLink>
+                             </li>
+                             <li>
+                                 <NavLink to="/trails">Trails</NavLink>
+                             </li>
+                             <li>
+                                 <NavLink to="/about">About</NavLink>
+                             </li>
+                             <li>
+                                 { this.props.isLoggedIn ? this.getSignOutButton : <NavLink to="/login">Log In</NavLink>}
+                             </li>
+                         </ul>
+                     </nav>
+                     <RouteContainer/>
+                 </div>
+             </HashRouter>
+         )
+     }
 }
+
+
+const mapStateToProps = (state) => ({
+    isLoggedIn: state.accountReducer.isLoggedIn
+})
+
+export default connect(mapStateToProps, ({ logOut }))(App)
 
