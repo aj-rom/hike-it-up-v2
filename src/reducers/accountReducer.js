194165f9d4ff489c369a30d16e1a0689db88b504
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-31 13:03:36 -0600

    switch to new state

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -2,11 +2,9 @@ const initialState = { id: null, errors: [], isLoggedIn: false }
 const accountReducer = ( state = initialState, action) => {
     switch (action.type) {
         case 'SIGN_UP':
-            console.log('Attempting signup', action.user)
-            return { user: action.user, isLoggedIn: true }
+            return { id: action.id, isLoggedIn: true }
         case 'LOG_IN':
-            console.log('Attempting login', action.user)
-            return { user: action.user, isLoggedIn: true }
+            return { id: action.id, isLoggedIn: true }
         case 'LOG_OUT':
             console.log('Logging Out, See ya!')
             return initialState
