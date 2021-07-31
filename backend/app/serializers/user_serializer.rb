3dc3487cae7bc7cf943c3cb36614f6558fb7c3ab
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-31 13:22:56 -0600

    use pluralize for a better return on trail count

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -4,6 +4,6 @@ class UserSerializer < ApplicationSerializer
   set_type :user
 
   attribute :trail_count do |obj|
-    obj.trails.nil? ? 0 : obj.trails.length
+    obj.trails.nil? ? 0 : "#{obj.trails.length} #{"Trail".pluralize(obj.trails.length)} Posted"
   end
 end
