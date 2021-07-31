a7b1ba9f355aadfee1b77734f433a3593fd4e11b
A.J. Romaniello <ajiellodev@gmail.com>
2021-07-31 13:06:51 -0600

    create userRedcuer.js

---
+++ src/reducers/userRedcuer.js
@@ -0,0 +1,24 @@
+const initialState = {
+    user: {
+        email: '',
+        trail_count: null
+    },
+    error: '',
+    isLoaded: false
+}
+
+const userReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case "LOADING":
+            return { ...state, isLoaded: false }
+        case "SET_USER":
+            return { ...state, user: action.user, isLoaded: true }
+        case "ERROR":
+            return { ...state, error: action.error }
+        default:
+            return initialState
+    }
+
+}
+
+export default userReducer
