f51e838c17c18e6e1a64ceb0d36b69f7a5864d73
A.J. Romaniello <ajiellodev@gmail.com>
2021-08-03 13:55:38 -0600

    create authentication token on sign up

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,8 +2,13 @@ class User < ApplicationRecord
   has_many :trails
 
   validates :email, email: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create do
+    self.auth_token = Devise.friendly_token(20)
+  end
 end
