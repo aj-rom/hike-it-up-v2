97060cfdc7dc55db4462c5af81a1f440cd3ed46e
A.J. Romaniello <ajiellodev@gmail.com>
2021-08-18 16:07:06 -0600

    handle nested attributes & refactor controller

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,35 +1,63 @@
 class TrailsController < ApplicationController
+  before_action :validate_user, only: %i[create destroy update]
+  before_action :find_trail, only: %i[show destroy update]
 
   def index
     trails = Trail.order('created_at DESC')
-    trails_json = TrailSerializer.new(trails).to_h
-    render json: trails_json
+    serialize trails
   end
 
   def show
-    trail = Trail.find_by(id: params[:id])
-    return render json: { error: 'Trail not found!' } unless trail
-
-    render json: TrailSerializer.new(trail).to_h
+    serialize @trail
   end
 
   def create
-    trail = Trail.create(trail_params)
-    if trail.id
-      redirect_to trail_path(trail)
+    trail = @user.trails.create(trail_params)
+    if trail.save
+      serialize trail
     else
+      puts "Creation failed..."
       render json: trail.errors.full_messages
     end
   end
 
-  def destroy; end
+  def destroy
+    if @user.id == @trail.user_id
+      @trail.destroy
+    else
+      render json: { error: ['You must be the owner of the trail to do this.'] }
+    end
+  end
 
   def update; end
 
   private
 
+  def serialize(trail)
+    render json: TrailSerializer.new(trail).to_h
+  end
+
+  def find_trail
+    puts "Finding trail"
+    @trail = Trail.find_by(id: params[:id])
+    render json: { error: 'Trail not found!' }, statues: 404 unless @trail
+    puts "Trail found"
+  end
+
+  def validate_user
+    puts "Finding User"
+    @user = User.find_by(auth_token: auth_params)
+    render json: { error: ['Invalid credentials.'] }, status: 402 unless @user
+    puts "User found!"
+  end
+
+  def auth_params
+    params.require(:auth_token)
+  end
+
   def trail_params
-    params.require(:trail).permit(:name, :description, :open_at, :close_at, :address, :user_id)
+    params.require(:trail)
+          .permit(:name, :description, :open_at, :close_at, address_attributes: %i[street city state zipcode])
   end
 
 end
