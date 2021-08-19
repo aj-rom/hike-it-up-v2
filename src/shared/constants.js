40ed9440f05ee88850b837f8613660e771b27df7
A.J. Romaniello <ajiellodev@gmail.com>
2021-08-19 14:56:54 -0600

    refactor to generalized header method

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,2 +1,10 @@
 export const BACKEND_URL = 'http://localhost:3001/'
 export const handleError = error => ({ type: 'ERROR', error: error })
+
+export const getHeader = (method = 'GET', body) => {
+    return {
+        method: method,
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(body)
+    }
+}
