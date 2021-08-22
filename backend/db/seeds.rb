8419f87606a6c821b5bc6d8eb509685366db9c82
A.J. Romaniello <ajiellodev@gmail.com>
2021-08-22 12:08:58 -0600

    Prepare for deployment

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
-user_one = User.create(email: 'user1@gmail.com', password: 'password', password_confirmation: 'password' )
-
-
-trail_one = {
-  name: 'Test Trail #1',
-  description: 'Test trail #1 description',
-  address: Address.create(street: 'Flagstaff Summit', city: 'Boulder', state: 'CO', zipcode: 80303),
-  open_at: '8:30',
-  close_at: '20:00'
-}
-
-user_one.trails.build(trail_one).save
-
-user_two = User.create(email: 'user2@gmail.com', password: 'password', password_confirmation: 'password' )
-
-trail_two = {
-  name: 'Test Trail #2',
-  description: 'Test Trail #2 description',
-  address: Address.create(street: 'Artist Point', city: 'Boulder', state: 'CO', zipcode: 80303),
-  open_at: '10:30',
-  close_at: '22:30'
-}
-
-user_two.trails.build(trail_two).save
+# user_one = User.create(email: 'user1@gmail.com', password: 'password', password_confirmation: 'password' )
+#
+#
+# trail_one = {
+#   name: 'Test Trail #1',
+#   description: 'Test trail #1 description',
+#   address: Address.create(street: 'Flagstaff Summit', city: 'Boulder', state: 'CO', zipcode: 80303),
+#   open_at: '8:30',
+#   close_at: '20:00'
+# }
+#
+# user_one.trails.build(trail_one).save
+#
+# user_two = User.create(email: 'user2@gmail.com', password: 'password', password_confirmation: 'password' )
+#
+# trail_two = {
+#   name: 'Test Trail #2',
+#   description: 'Test Trail #2 description',
+#   address: Address.create(street: 'Artist Point', city: 'Boulder', state: 'CO', zipcode: 80303),
+#   open_at: '10:30',
+#   close_at: '22:30'
+# }
+#
+# user_two.trails.build(trail_two).save
