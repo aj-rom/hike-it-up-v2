ab112b5f16e6842dc112c0a192732a4e89da3108
A.J. Romaniello <ajiellodev@gmail.com>
2021-08-22 12:58:49 -0600

    Switch to heroku URL

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'http://localhost:3001/'
+export const BACKEND_URL = 'https://hike-it-up-v2.herokuapp.com/'
 export const handleError = error => ({ type: 'ERROR', error: error })
 
 export const getHeader = (method = 'GET', body) => {
